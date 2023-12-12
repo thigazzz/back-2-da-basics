75b5f53a0569dd007f7baec6a31debb79be40d08
constThiagoSilva <thiago.p.dasilva2005@gmail.com>
2023-12-12 08:12:05 -0300

    [DS] Trees Explanation

---
+++ algotithm/ds/tree.py
@@ -0,0 +1,16 @@
+"""
+
+Árvores são estruturas formada por nós que se conectam entre si.
+Diferente das Linked Lists, cada nó de uma árvore pode se conectar
+com um ou mais nós
+
+- Uma árvore é formada por um Nó 'ROOT', que é o ponto de partida para os demais nós.
+- O antecessor de um Nó é denominado "Parent"
+- O sucesso de um Nó é denominado "Child"
+- O Nó ROOT não pode ter um Parent
+- Um Nó não pode ser apontado mais de uma vez
+- Cada filho pode ser representado por uma camada, level ou grau
+
+# Tipos de Árvores
+- Binary Trees: São arvores formada por dois filhos cada Nó (left e right)
+"""
